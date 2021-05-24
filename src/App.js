35c31f62fa50d1ba18e7070208defeca7373eb1d
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-05-24 17:24:12 +0200

    pass options to Dropdown.js via props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Dropdown from "./Dropdown";
 import "./App.css";
 
 function App() {
+  const data = [
+    { value: 1, name: "A" },
+    { value: 2, name: "B" },
+    { value: 3, name: "C" },
+  ];
+
   return (
     <div className="App">
-      <Dropdown />
+      <Dropdown options = {data} />
     </div>
   );
 }
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,16 +1,10 @@
 import React from "react";
 
-const Dropdown = () => {
-  const data = [
-    { value: 1, name: "A" },
-    { value: 2, name: "B" },
-    { value: 3, name: "C" },
-  ];
-
+const Dropdown = ({ options }) => {
   return (
     <div>
       <select>
-        {data.map((item, idx) => (
+        {options.map((item, idx) => (
           <option key={idx} value={item.value}>
             {item.name}
           </option>
