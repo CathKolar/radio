3fad8d7352aa30e27e9ac23714992bf7c2fd7533
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-05-25 09:58:06 +0200

    start work on spotify API call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dropdown from "./Dropdown";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
+  const spotify = Credentials();
+
   const data = [
     { value: 1, name: "A" },
     { value: 2, name: "B" },
     { value: 3, name: "C" },
   ];
+
+  const [token, setToken] = useState("");
+  const [generes, setGeneres] = useState([]);
+
+  useEffect(() => {
+    axios("https://accounts.spotify.com/api/token", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
+      },
+      data: "grant_type=client_credentials",
+      method: "POST",
+    }).then((tokenResponse) => {
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+    });
+  });
 
   return (
     <form onSubmit={() => {}}>
